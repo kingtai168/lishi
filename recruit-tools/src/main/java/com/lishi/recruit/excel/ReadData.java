efa14c54fc525bc60175b83efe9b49eb9a32fe2c
Administrator <Administrator@PC-20160920WBHD.tempus.cn>
2016-10-27 18:03:27 +0800

    f

--- recruit-tools/src/main/java/com/lishi/recruit/excel/ReadData.java
+++ recruit-tools/src/main/java/com/lishi/recruit/excel/ReadData.java
@@ -1,8 +1,11 @@
 package com.lishi.recruit.excel;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -26,7 +29,41 @@ import com.lishi.recruit.tools.StringUtil;
  
 public class ReadData {
 	    
-	  
+	public static void contentToTxt(String filePath,List<String> list) {                                    
+//	       String str = new String(); //原有txt内容                                                       
+	       String s1 = new String();//内容更新                                                            
+	       try {                                                                                          
+	           File f = new File(filePath);                                                               
+	           if (f.exists()) {                                                                          
+	               System.out.print("文件存在");                                                          
+	           } else {                                                                                   
+	               System.out.print("文件不存在");                                                        
+	               f.createNewFile();// 不存在则创建                                                      
+	           }                                                                                          
+	           BufferedReader input = new BufferedReader(new FileReader(f));                              
+	                                                                                                      
+//	            while ((str = input.readLine()) != null) {                                                
+//	                s1 += str + "\n";                                                                     
+//	            }                           
+	           if(null!=list&&!list.isEmpty())
+	           {
+	        	   for (String str : list) {
+	        		   s1 += str + "\n";    
+				    }
+	           }
+	            System.out.println(s1);                                                                   
+	            input.close();                                                                            
+//	            s1 += content;                                                                            
+	                                                                                                      
+	            BufferedWriter output = new BufferedWriter(new FileWriter(f));                            
+	            output.write(s1);                                                                         
+	            output.close();                                                                           
+	        } catch (Exception e) {                                                                       
+	            e.printStackTrace();                                                                      
+	                                                                                                      
+	        }                                                                                             
+	    }                                                                                                 
+
 	    
 	 public static void readTxtFile(String filePath){
 	        try {
